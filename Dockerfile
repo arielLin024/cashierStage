38ef9f7503204270a176f104de1191996a6ecf2b
ariel.lin <ariel.lin@ecpay.com.tw>
2024-02-07 16:32:36 +0800

    edit dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,20 @@
 # 使用 node 20 作為基本映像
 FROM node:20
 
-# 安裝必要的依賴項
+# 安裝必要的依賴項並設置時區
 RUN apt-get update && \
     apt-get install -y \
-    xvfb \
-    libgtk-3-0 \
-    libnotify-dev \
-    libgconf-2-4 \
-    libnss3 \
-    libxss1 \
-    libasound2 \
-    libxtst6 \
-    xauth \
-    libgbm-dev \
-    libatk1.0-0 && \
+        xvfb \
+        libgtk-3-0 \
+        libnotify-dev \
+        libgconf-2-4 \
+        libnss3 \
+        libxss1 \
+        libasound2 \
+        libxtst6 \
+        xauth \
+        libgbm-dev \
+        libatk1.0-0 && \
     ln -fs /usr/share/zoneinfo/Asia/Taipei /etc/localtime && \
     dpkg-reconfigure -f noninteractive tzdata
 
@@ -22,31 +22,22 @@ RUN apt-get update && \
 WORKDIR /app
 
 # 複製項目文件到工作目錄
-# COPY . /app
 COPY . .
 
 # 複製整個 assets 目錄到容器中的 /app/assets
 COPY ./report/assets /app/assets
 
-# # 安裝 Cypress 和相關依賴
-# RUN npm install
-# RUN npx cypress install --force
-
 # 安裝依賴項
 COPY package*.json ./
 RUN npm install && npx cypress install --force
 
-# # 創建存放 Cypress 測試結果的目錄
-# RUN mkdir -p /app/cypress/results/.jsons
-
 # 創建存放 Cypress 測試結果的目錄
 RUN mkdir -p /app/cypress/results
-
-# # 複製報告檔案到容器中的目錄
-# COPY cypress/results/.jsons /app/cypress/results/.jsons
 
 # 安裝 mochawesome-merge
 RUN npm install -g mochawesome-merge
 
 # 執行 Cypress 測試
-CMD npx cypress run && node newSortsonByDate.js && npx marge "mochawesome.json" -f report -o report
+CMD npx cypress run && \
+    node newSortsonByDate.js && \
+    npx marge "mochawesome.json" -f report -o report
--- cypress/e2e/testData.js
+++ cypress/e2e/testData.js
@@ -16,33 +16,38 @@ export const cartSelect = {
 };
 export const imformation = {
   nextStep: '//*[@id="checkout-root"]/div/div/div[2]/div/a', // cilck to checkout
-  shippingMethods: ":nth-child(2) > .sdtb-box > .sdtb-radio-list > :nth-child(5) > .radio", // shipping Methods
+  shippingMethods:
+    ":nth-child(2) > .sdtb-box > .sdtb-radio-list > :nth-child(5) > .radio", // shipping Methods
   recipientNameField: ".sfb-w1 > dd > .sfb-inp",
   recipientPhoneField: ".sfb-w2 > dd > .sfb-inp",
   recipientMailField: ".sfb-w3 > dl > dd > .sfb-inp",
-  recipientName: "candy",
-  recipientPhone: "0900555777",
+  recipientName: "陳小美",
+  recipientPhone: "0988166166",
   recipientMail: "aabcc321@ecpay.com.tw",
   countryField: "select",
-  country: "HSZ",
+  country: "TPE",
   districtField: ".sfb-w2 > span > .def-select > select",
-  district: "NTH",
+  district: "NGG",
   addressField: ".sdtb-form-box > .sfb-addr > .sfb-w4 > span > .sfb-inp",
-  address: "北北北北北北北北北北北北北北北北北",
+  address: "三重路19-2號6樓",
   overSea: ".sdtb-box > :nth-child(2) > .radio",
   // 非即時交易
   ordereNotRecipientBTN: ".checkbox",
-  orderNameField: ".sdtb-form-box:nth-child(3) > span > .sfb-w1 > dd > .sfb-inp",
-  orderPhoneField: ".sdtb-form-box:nth-child(3) > span > .sfb-w2 > dd > .sfb-inp",
+  orderNameField:
+    ".sdtb-form-box:nth-child(3) > span > .sfb-w1 > dd > .sfb-inp",
+  orderPhoneField:
+    ".sdtb-form-box:nth-child(3) > span > .sfb-w2 > dd > .sfb-inp",
   orderMailField: ".sdtb-form-box:nth-child(3) > .sfb-w3 > dl > dd > .sfb-inp",
-  orderNeme: "emily",
-  orderPhone: "0955333444",
+  orderNeme: "陳小美",
+  orderPhone: "0988166166",
   orderMail: "aabcc321@ecpay.com.tw",
 };
 export const payment = {
-  methodCreditCard: ":nth-child(6) > .sdtb-box > .sdtb-radio-list > :nth-child(1) > .radio", //creditCard
+  methodCreditCard:
+    ":nth-child(6) > .sdtb-box > .sdtb-radio-list > :nth-child(1) > .radio", //creditCard
   methodATM: ":nth-child(9) > .radio",
-  invoice: ":nth-child(7) > .sdtb-box > .sdtb-radio-list > :nth-child(1) > .radio",
+  invoice:
+    ":nth-child(7) > .sdtb-box > .sdtb-radio-list > :nth-child(1) > .radio",
   carrier: ":nth-child(3) > .sdtb-radio-list > :nth-child(1) > .radio",
   confirmBtn: ".btn-main",
   creditCardNumField_1: "#CCpart1",
@@ -53,18 +58,22 @@ export const payment = {
   creditCardNum_3: "2222",
   creditCardNumField_4: "#CCpart4",
   creditCardNum_4: "2222",
-  creditmmField: ".pay-tab-form > .pay-card-date > :nth-child(2) > div > #creditMM",
-  creditmmNum: "11",
-  credityyField: ".pay-tab-form > .pay-card-date > :nth-child(2) > div > #creditYY",
-  credityyNum: "25",
-  securityCodeField:".pay-tab-form > .pay-card-date > .pay-card-ccvb > dl > dd > #CreditBackThree",
+  creditmmField:
+    ".pay-tab-form > .pay-card-date > :nth-child(2) > div > #creditMM",
+  creditmmNum: "12",
+  credityyField:
+    ".pay-tab-form > .pay-card-date > :nth-child(2) > div > #creditYY",
+  credityyNum: "28",
+  securityCodeField:
+    ".pay-tab-form > .pay-card-date > .pay-card-ccvb > dl > dd > #CreditBackThree",
   securityCodeNum: "222",
   cardholderField: ":nth-child(6) > dd > #CCHolderTemp",
-  cardholderName: "Backy",
+  cardholderName: "王小明",
   cardholderPhoneField: ".pay-tab-form > .ptf-dfe-phone > dd > #CellPhoneCheck",
-  cardholderPhoneNum: "0900555777",
+  cardholderPhoneNum: "0988188188",
   paynowBtn: "#CreditPaySubmit",
-  checkBtnClose: "#Credit > .simplert > .simplert__content > .simplert__footer > #btnClose",
+  checkBtnClose:
+    "#Credit > .simplert > .simplert__content > .simplert__footer > #btnClose",
   paySubmit: "#CreditPaySubmit",
   btnConfirm: "#btnConfirm",
   orderCheck: "/html/body/div[2]/div/div/div/div/div[2]",
